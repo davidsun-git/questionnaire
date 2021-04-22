e532eb487d28f444364a90198854a7b21332623b
bsnpc1l <David-LW.Sun@aia.com>
2021-04-22 21:17:34 +0800

    initial first three questions

--- survey.js
+++ survey.js
@@ -15,19 +15,23 @@ Vue.component("survey", {
       type: Number,
       default: 1,
     },
-    sex: {
+    a: {
       type: Number,
       default: Infinity,
     },
-    hobby: {
-      type: Array,
-      default: [],
+    b: {
+      type: Number,
+      default: Infinity,
     },
-    introduce: {
-      type: String,
-      default: "",
-      minLength: 100,
+    c: {
+      type: Number,
+      default: Infinity,
     },
+    // introduce: {
+    //   type: String,
+    //   default: "",
+    //   minLength: 100,
+    // },
     activeColor: {
       type: String,
       default: "activeColor",
@@ -44,8 +48,7 @@ Vue.component("survey", {
     "   </div>" +
     '   <my-button v-model="currentValue">' +
     '        <button :class="activeColor" v-if="currentValue <= surveys.length && currentValue != 1" @click="upStep(currentValue-2)">上一步</button>' +
-    '        <button :class="[nextStepBtn?disableColor:activeColor]" v-if="currentValue != surveys.length" :disabled="nextStepBtn" @click="nextStep(currentValue)">下一步</button>' +
-    '        <button @click="reset">重置</button>' +
+    '        <button :class="[nextStepBtn?disableColor:activeColor]" v-if="currentValue != surveys.length + 1" :disabled="nextStepBtn" @click="nextStep(currentValue)">下一步</button>' +
     '        <button v-if="currentValue == surveys.length" :disabled="submitBtn" @click="submit">提交</button>' +
     "    </my-button>" +
     "</div>",
@@ -58,32 +61,36 @@ Vue.component("survey", {
       this.checkStatus(id);
       this.currentValue--;
     },
-    reset: function () {
-      this.$parent.sex = "";
-      this.$parent.hobby = [];
-      this.$parent.introduce = "";
-      alert("重置成功");
-    },
+    // reset: function () {
+    //   this.$parent.a = "";
+    //   this.$parent.b = [];
+    //   this.$parent.introduce = "";
+    //   alert("重置成功");
+    // },
     submit: function () {
-      let sex = this.$parent.sex;
-      let hobby = this.$parent.hobby;
-      let introduce = this.$parent.introduce;
+      let a = this.$parent.a;
+      let b = this.$parent.b;
+      let c = this.$parent.c;
       alert(
-        "提交:\rsex:" + sex + "\rhobby:" + hobby + "\rintroduce" + introduce
+        "提交:\ra:" + a + "\rb:" + b + "\rintroduce" + introduce
       );
     },
     //优化下一步按钮是否置灰问题
     checkStatus: function (id) {
-      if ("hobby" == this.surveys[id].topic) {
-        debugger;
-        if (this.hobby.length > 0) {
+      if ("b" == this.surveys[id].topic) {
+        if (this.b.length > 0) {
           this.nextStepBtn = false;
         } else {
           this.nextStepBtn = true;
         }
-      } else if ("sex" == this.surveys[id].topic) {
-        debugger;
-        if (this.sex.length > 0) {
+      } else if ("a" == this.surveys[id].topic) {
+        if (this.a.length > 0) {
+          this.nextStepBtn = false;
+        } else {
+          this.nextStepBtn = true;
+        }
+      } else if ("c" == this.surveys[id].topic) {
+        if (this.c.length > 0) {
           this.nextStepBtn = false;
         } else {
           this.nextStepBtn = true;
@@ -98,23 +105,29 @@ Vue.component("survey", {
     currentValue: function (val) {
       this.$emit("input", val);
     },
-    sex: function () {
-      debugger;
-      if (this.sex.length > 0) {
+    a: function () {
+      debugger
+      if (this.a.length > 0) {
         this.nextStepBtn = false;
       }
     },
-    hobby: function () {
-      debugger;
-      if (this.hobby.length > 0) {
+    b: function () {
+      if (this.b.length > 0) {
         this.nextStepBtn = false;
       }
     },
-    introduce: function (val) {
-      if (val.length > 100) {
+    c: function () {
+      debugger;
+      if (this.c.length > 0) {
+        this.nextStepBtn = false;
         this.submitBtn = false;
       }
     },
+    // introduce: function (val) {
+    //   if (val.length > 100) {
+    //     this.submitBtn = false;
+    //   }
+    // },
     nextStepBtn: function () {},
   },
 });
